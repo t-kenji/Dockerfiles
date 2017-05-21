94bf87059fc26324c52f316d057e8f06309b76ca
t-kenji <protect.2501@gmail.com>
2017-05-22 07:57:47 +0900

    Change: autorun errbot

--- Errbot/run.sh
+++ Errbot/run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-docker run -ti \
-           --name errbot\
-           tkenji/errbot \
+NAME=errbot
+MOUNT_POINT=${HOME}/.docker-containers/volumes/${NAME}
+
+docker run -d \
+           --name ${NAME} \
+           --link letschat:chat \
+           -v ${MOUNT_POINT}/data:/var/lib/errbot/data \
+           -e 'ERRBOT_LCB_HOSTNAME=chat' \
+           -e 'ERRBOT_LCB_TOKEN=NTkyMjAzOTM1ZDAyZWM4MDAwODQ4ZDVhOjZlZDRiNzhjYTYwMzE2NTNlYjQxODczNGI4ODZjZWY0NDgwMjM4MjhhZDBhNTk3Yw==' \
+           -e 'ERRBOT_LCB_ROOMS=59214e325d02ec8000848d58' \
+           -e 'ERRBOT_LCB_ADMINS=@ktakahashi' \
+           -e 'ERRBOT_LCB_NAME=errbot' \
+           tkenji/${NAME} \
            $@
---
+++ Errbot/script/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd $HOME
+
+if [ ! -d errbot-moogle ]; then
+	git clone https://github.com/t-kenji/errbot-moogle.git
+	sed -i "s/BOT_DATA_DIR = .\+/BOT_DATA_DIR = r'\/var\/lib\/errbot\/data'/" errbot-moogle/config.py
+else
+	echo "[INFO] errbot-moogle already exists."
+fi
+
+if [ ! -d venv ]; then
+	python -m venv venv
+	./venv/bin/pip install errbot socketio-client pytz crontab
+else
+	echo "[INFO] venv already exists."
+fi
+
+exec $@
