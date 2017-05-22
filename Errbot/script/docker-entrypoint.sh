65f163be4e10bd953dc97726f998cb3e58f77b81
t-kenji <protect.2501@gmail.com>
2017-05-23 08:19:36 +0900

    Fix: ErrBot start failed

--- Errbot/script/docker-entrypoint.sh
+++ Errbot/script/docker-entrypoint.sh
@@ -4,6 +4,7 @@ cd $HOME
 
 if [ ! -d errbot-moogle ]; then
 	git clone https://github.com/t-kenji/errbot-moogle.git
+	sed -i "s/ROOTDIR = .\+/ROOTDIR = r'\/root\/errbot-moogle'/" errbot-moogle/config.py
 	sed -i "s/BOT_DATA_DIR = .\+/BOT_DATA_DIR = r'\/var\/lib\/errbot\/data'/" errbot-moogle/config.py
 else
 	echo "[INFO] errbot-moogle already exists."
