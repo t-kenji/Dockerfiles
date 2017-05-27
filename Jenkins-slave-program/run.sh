2c5a71771ec3390dd7e168ae549f1be21e10620f
t-kenji <protect.2501@gmail.com>
2017-05-28 04:52:15 +0900

    Change: Improve build scripts

--- Jenkins-slave-program/run.sh
+++ Jenkins-slave-program/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -d --name jenkins-slave-program-1 tkenji/jenkins-slave-program
+NAME=jenkins-slave-program
+MOUNT_POINT=${HOME}/.docker-containers/volumes/${NAME}-1
+
+docker run -d \
+           --name ${NAME}-1 \
+           -v ${MOUNT_POINT}/ssh:/home/jenkins/.ssh \
+           tkenji/${NAME} \
+           $@
