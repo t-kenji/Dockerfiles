25bd006091fa0d088754ed9865b0b546e5b527fa
t-kenji <protect.2501@gmail.com>
2017-05-28 04:49:03 +0900

    Fix: slave-node connection

--- Jenkins-master/run.sh
+++ Jenkins-master/run.sh
@@ -8,6 +8,7 @@ docker run -d \
            --name ${NAME} \
            -p 8080:8080 \
            -p 50000:50000 \
+           --link jenkins-slave-program-1:slave-program-1 \
            -v ${MOUNT_POINT}/jenkins_home:/var/jenkins_home \
            tkenji/${NAME} \
            $@
--- Jenkins-master/script/docker-entrypoint.sh
+++ Jenkins-master/script/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 if [ ! -d $JENKINS_HOME/.ssh ]; then
 	mv $HOME/.ssh $JENKINS_HOME
+	ssh-keyscan -H slave-program-1 >> /var/jenkins_home/.ssh/known_hosts 2>&1
 	/usr/local/bin/plugins.sh /usr/share/jenkins/ref/plugins.txt
 	chown jenkins:jenkins -R $JENKINS_HOME
 fi
