e5c30b1d9fab554983ba68ea321e2b8e10b488c8
t-kenji <protect.2501@gmail.com>
2017-06-01 10:50:48 +0900

    Cleanup: improved build / run scripts

--- Trac/run.sh
+++ Trac/run.sh
@@ -5,7 +5,6 @@ MOUNT_POINT=${HOME}/.docker-containers/volumes/${NAME}
 
 docker run -d \
            --name ${NAME} \
-           -p 8001:8001 \
            -v ${MOUNT_POINT}/data:/var/lib/trac \
            tkenji/${NAME} \
            $@
--- Trac/script/docker-entrypoint.sh
+++ Trac/script/docker-entrypoint.sh
@@ -42,6 +42,7 @@ git+https://github.com/t-kenji/trac-keyword-badges-plugin.git
 git+https://github.com/t-kenji/trac-letschat-plugin.git
 git+https://github.com/t-kenji/trac-mension-plugin.git
 git+https://github.com/t-kenji/trac-mikeneko-theme.git
+git+https://github.com/t-kenji/trac-multi-select-field-plugin.git
 git+https://github.com/t-kenji/trac-perm-redirect-plugin.git
 git+https://github.com/t-kenji/trac-privatetickets-plugin.git
 git+https://github.com/t-kenji/trac-sectionedit-plugin.git
@@ -52,13 +53,38 @@ git+https://github.com/t-kenji/trac-ticket-relations-plugin.git
 git+https://github.com/t-kenji/trac-ticket-template-plugin.git
 git+https://github.com/t-kenji/trac-xml-rpc-plugin.git
 __EOS__
+	./venv/bin/pip install setuptools==33.1.1
 	./venv/bin/pip install -r ${requirements}
 else
 	echo "[INFO] venv already exists."
 fi
 
-if [ ! -d /var/lib/trac/deploy ]; then
-	./venv/bin/trac-admin /var/lib/trac/sites/trac-plugin-test deploy /var/lib/trac/deploy
+source ./venv/bin/activate
+
+SITES_DIR=/var/lib/trac/sites
+if [ ! -d $SITES_DIR ]; then
+	mkdir -p $SITES_DIR
+	trac-admin $SITES_DIR/trac-project-sample initenv sample sqlite:db/trac.db
+	echo "trac-project-sample:8001" >> $SITES_DIR/servers.txt
 fi
+
+DEPLOY_DIR=/var/lib/trac/deploy
+if [ ! -d $DEPLOY_DIR ]; then
+	trac-admin $SITES_DIR/trac-project-sample deploy $DEPLOY_DIR
+fi
+
+for l in `cat $SITES_DIR/servers.txt`; do
+	cols=(`echo $l | tr -s ':' ' '`)
+	project=${cols[0]}
+	port=${cols[1]}
+	cat << __EOS__ > /etc/supervisor.d/${project}.ini
+[program:${project}]
+directory = /root
+command = /root/venv/bin/python /root/bootstrap.py ${project} ${port}
+autorestart = true
+redirect_stderr = true
+stdout_logfile = /var/log/${project}.log
+__EOS__
+done
 
 exec "$@"
