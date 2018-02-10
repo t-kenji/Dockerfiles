8d194d80dc1690dcccb371c6c1154736adc2bd8d
t-kenji <protect.2501@gmail.com>
2018-02-10 14:52:50 +0900

    Fix: Janus container

--- Janus/script/docker-entrypoint.sh
+++ Janus/script/docker-entrypoint.sh
@@ -2,18 +2,18 @@
 
 sed -e 's/cert_pem = .\+/cert_pem = \/usr\/share\/certs\/cert.crt/' \
     -e 's/cert_key = .\+/cert_key = \/usr\/share\/certs\/cert.key/' \
-    -i /usr/etc/janus/janus.cfg
+    -i /etc/janus/janus.cfg
 sed -e 's/^;interface = /interface = /' \
     -e 's/^https = no/https = yes/' \
     -e 's/^;secure_port = /secure_port = /' \
     -e 's/^;secure_interface = /secure_interface = /' \
     -e 's/cert_pem = .\+/cert_pem = \/usr\/share\/certs\/cert.crt/' \
     -e 's/cert_key = .\+/cert_key = \/usr\/share\/certs\/cert.key/' \
-    -i /usr/etc/janus/janus.transport.http.cfg
-sed -e 's/^wws = no/wws = yes/' \
+    -i /etc/janus/janus.transport.http.cfg
+sed -e 's/^wss = no/wss = yes/' \
     -e 's/^;wss_port = /wss_port = /' \
     -e 's/cert_pem = .\+/cert_pem = \/usr\/share\/certs\/cert.crt/' \
     -e 's/cert_key = .\+/cert_key = \/usr\/share\/certs\/cert.key/' \
-    -i /usr/etc/janus/janus.transport.websockets.cfg
+    -i /etc/janus/janus.transport.websockets.cfg
 
 exec "$@"
